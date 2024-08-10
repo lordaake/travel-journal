bc9a701ea47492bb1a2ab9fb83aa07543b1f0e8e
lordaake <lordaake@protonmail.com>
2024-08-10 09:48:12 +0200

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,23 @@
+import Data from "./Data"
+
+export default function Body(props) {
+    return (
+        <div className="Body--container">
+            <img src={props.destination.imageUrl} className="destination--img" />
+            <div className="RestofBody--container">
+                <div className="url--location">
+                    <p>{props.destination.location}</p>
+                    <a href={props.destination.googleMapsUrl} target="_blank" rel="noopener noreferrer">
+                        View on Google Maps
+                    </a>
+                </div>
+                <h2>{props.destination.title}</h2>
+                <div className="startToEnd">
+                    <p>{props.destination.startDate} </p>
+                    <p>{props.destination.endDate}</p>
+                </div>
+                <p className="description">{props.destination.description}</p>
+            </div>
+        </div>
+    )
+}
